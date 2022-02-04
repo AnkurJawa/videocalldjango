a6b73a863bec68874c70d7d9c7eeef5e33666912
AtulKumar18103122 <atulkumar.be18cse@pec.edu.in>
2022-02-04 21:35:16 +0530

    deploy 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import dotenv
 from pathlib import Path
+import os
 
 dotenv.load_dotenv()
 
@@ -123,4 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
